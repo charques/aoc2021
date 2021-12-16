241452a830f65f39b1c0006c5cf515b5b9f88240
Carlos Hernandez <carlosherandezarques@gmail.com>
2021-12-16 21:57:59 +0100

    day 15 - second part attempt

--- day15/index.js
+++ day15/index.js
@@ -6,6 +6,41 @@ function createMatrix(input) {
     return matrix;
 }
 
+function createExtendedMatrix(input, loops) {
+    var result = [];
+    input.forEach((element, index) => {
+        var t = element.split("").map(x => parseInt(x));
+        var extendedRow = createExtendedRow(t, loops);
+        var t2 = [...extendedRow];
+        for(var i = 0; i < loops; i++) {
+            if(i > 0) {
+                t2 = t2.map(x => {
+                    if(x == 9) x = 0;
+                    return x+1;
+                });
+            }
+            result[(i*input.length)+index] = t2;
+        }
+        //console.log(extendedRow);
+    });
+    return result;
+}
+
+function createExtendedRow(origin, loops) {
+    var rtotal = [];
+    var t = [...origin];
+    for(var i = 0; i < loops; i++) {
+        if(i > 0) {
+            t = t.map(x => {
+                if(x == 9) x = 0;
+                return x+1;
+            });
+        }
+        rtotal = rtotal.concat(t);
+    }
+    return rtotal;
+}
+
 function riskCalc(matrix) {
     var solutions = {};
     for(var y = matrix.length-1; y >= 0; y--) {
@@ -53,6 +88,14 @@ function calcRiskLevel(input) {
     return min - matrix[0][0];
 }
 
+function calcRiskLevel2(input) {
+    let matrix = createExtendedMatrix(input, 5);
+    var result = riskCalc(matrix);
+    let min = findMin(result);
+    return min - matrix[0][0];
+}
+
 module.exports = {
-    calcRiskLevel: calcRiskLevel
+    calcRiskLevel: calcRiskLevel,
+    calcRiskLevel2: calcRiskLevel2
 };
--- test/day15.spec.js
+++ test/day15.spec.js
@@ -25,4 +25,32 @@ describe("Day 15: Chiton", function() {
     expect(result).to.equal(673);
   });
 
+  it("test calcRiskLevel2 - example 0a", function() {
+    const file = fs.readFileSync("./day15/input15-0a.txt", "utf-8");
+    const input = file.split("\n");
+    const result = day15.calcRiskLevel2(input);
+    expect(result).to.equal(134);
+  });
+
+  it("test calcRiskLevel2 - example 0b", function() {
+    const file = fs.readFileSync("./day15/input15-0b.txt", "utf-8");
+    const input = file.split("\n");
+    const result = day15.calcRiskLevel2(input);
+    expect(result).to.equal(83);
+  });
+
+  it("test calcRiskLevel2 - example 0a", function() {
+    const file = fs.readFileSync("./day15/input15-0.txt", "utf-8");
+    const input = file.split("\n");
+    const result = day15.calcRiskLevel2(input);
+    expect(result).to.equal(315);
+  });
+
+  it("test calcRiskLevel2 - example 1", function() {
+    const file = fs.readFileSync("./day15/input15-1.txt", "utf-8");
+    const input = file.split("\n");
+    const result = day15.calcRiskLevel2(input);
+    expect(result).to.equal(2908);
+  });
+
 });
