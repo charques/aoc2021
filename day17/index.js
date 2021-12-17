609ae067b9ff9c2249264f7809b3b5f2a18354f2
Carlos Hernandez <carlosherandezarques@gmail.com>
2021-12-17 20:50:35 +0100

    day 17 - second part

--- day17/index.js
+++ day17/index.js
@@ -1,18 +1,19 @@
 
-function coordInside(target, x, y) {
-    // target area: x=20..30, y=-10..-5
-    // target area: x=282..314, y=-80..-45
+const HIT_TARGET = 1;
+const OUT_MISS_TARGET = 2;
+const OUT_FAR_FROM_TARGET = 0;
 
+function coordInside(target, x, y) {
     // IN
     if((x >= target.x1) && (x <= target.x2) && 
         (y <= target.y1) && (y >= target.y2)) {
-        return 1;
+        return HIT_TARGET;
     }
     // BEYOND
     if((x > target.x2) || (y < target.y2)) {
-        return 2;
+        return OUT_MISS_TARGET;
     }
-    return 0;
+    return OUT_FAR_FROM_TARGET;
 }
 
 function calc(target, vX, vY) {
@@ -21,17 +22,21 @@ function calc(target, vX, vY) {
     var x = 0;
     var y = 0;
     var step = 0;
+    var xInc = 0;
+    var yInc = 0;
     do {
-        var xx = ((vX-step) >= 0) ? vX-step : 0;
-        x = x + xx;
-        y = y + (vY-step);
+        xInc = ((vX-step) >= 0) ? vX-step : 0;
+        yInc = vY-step;
+        x = x + xInc;
+        y = y + yInc;
         result.push([x,y]);
         cordCheck = coordInside(target, x, y);
         if(cordCheck > 0) break;
         step++;
     } while(true);
-    if(cordCheck == 1) {
-        // max High
+
+    if(cordCheck == HIT_TARGET) {
+        // calc max high
         var max = result.reduce((prev, curr) => {
             return (curr[1] > prev) ? curr[1] : prev;
         }, 0);
@@ -43,7 +48,7 @@ function calc(target, vX, vY) {
     return null;
 }
 
-function highestY(x1, x2, y1, y2, SIZE) {
+function highestY(x1, x2, y1, y2) {
     const target = {
         x1: x1,
         x2: x2,
@@ -53,6 +58,8 @@ function highestY(x1, x2, y1, y2, SIZE) {
 
     var r = null;
     var results = [];
+
+    const SIZE = 200;
 
     for(var vY = 0; vY < SIZE; vY++) {
         for(var vX = 0; vX < SIZE; vX++) {
@@ -69,6 +76,34 @@ function highestY(x1, x2, y1, y2, SIZE) {
     return max;
 }
 
+function numHits(x1, x2, y1, y2) {
+    const target = {
+        x1: x1,
+        x2: x2,
+        y1: y1,
+        y2: y2
+    };
+
+    var r = null;
+    var results = [];
+
+    var INIT_Y = -250;
+    var SIZE_Y = 500;
+    var SIZE_X = 700;
+
+    for(var vY = INIT_Y; vY < SIZE_Y; vY++) {
+        for(var vX = 0; vX < SIZE_X; vX++) {
+            r = calc(target, vX, vY);
+            if(r != null) {
+                results.push(r);
+            }
+        }
+    }
+    
+    return results.length;
+}
+
 module.exports = {
-    highestY: highestY
+    highestY: highestY,
+    numHits: numHits
 };
--- test/day17.spec.js
+++ test/day17.spec.js
@@ -5,16 +5,22 @@ var day17 = require("../day17/index");
 describe("Day 17: Trick Shot", function() {
 
   it("test highestY - example 0", function() {
-    const file = fs.readFileSync("./day17/input17-0.txt", "utf-8");
-    const input = file.split("\n");
-    const result = day17.highestY(20, 30, -5, -10, 100);
+    const result = day17.highestY(20, 30, -5, -10);
     expect(result).to.equal(45);
   });
 
-  it("test highestY - example 1", function() {
-    const file = fs.readFileSync("./day17/input17-0.txt", "utf-8");
-    const input = file.split("\n");
-    const result = day17.highestY(282, 314, -45, -80, 200);
+  /*it("test highestY - example 1", function() {
+    const result = day17.highestY(282, 314, -45, -80);
     expect(result).to.equal(3160);
   });
+
+  it("test highestY - example 0", function() {
+    const result = day17.numHits(20, 30, -5, -10);
+    expect(result).to.equal(112);
+  });
+
+  it("test highestY - example 0", function() {
+    const result = day17.numHits(282, 314, -45, -80);
+    expect(result).to.equal(1928);
+  });*/
 });
